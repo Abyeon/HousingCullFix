faf60eece6ee8ba23c88b3342500309877471f0c
Abyeon <wpseals@gmail.com>
2026-04-30 18:59:51 -0400

    Change fix to switching GraphicsConfig->IsInside to true. This has the side benefit of fixing void lighting.

--- HousingCullFix/CullHook.cs
+++ HousingCullFix/CullHook.cs
@@ -7,17 +7,23 @@ using FFXIVClientStructs.FFXIV.Client.Graphics.Render;
 using FFXIVClientStructs.FFXIV.Client.Graphics.Scene;
 using FFXIVClientStructs.FFXIV.Client.LayoutEngine.Layer;
 using HousingCullFix.Structs;
+using InteropGenerator.Runtime;
 
 namespace HousingCullFix;
 
 public unsafe class CullHook : IDisposable
 {
     //char __fastcall sub_1404536B0(__int64 a1, int a2, unsigned int a3, float a4)
-    private delegate byte CullDelegate(BgObject* a1, int a2, uint a3, float a4);
+    // private delegate byte CullDelegate(BgObject* a1, int a2, uint a3, float a4);
+    //
+    // [Signature("40 55 56 57 41 55 41 56 48 8D 6C 24 ?? 48 81 EC ?? ?? ?? ?? 0F 29 B4 24", DetourName = nameof(CullDetour))]
+    // private Hook<CullDelegate>? cullHook;
     
-    [Signature("40 55 56 57 41 55 41 56 48 8D 6C 24 ?? 48 81 EC ?? ?? ?? ?? 0F 29 B4 24", DetourName = nameof(DetourCull))]
-    private Hook<CullDelegate>? cullHook;
+    private delegate void LoadIndoorsDelegate(IntPtr a1, int a2, CStringPointer a3, IntPtr a4, int a5, int a6, int a7, IntPtr a8, int a9);
 
+    [Signature("40 53 48 83 EC ?? 8B 44 24 ?? 48 8B D9 89 41", DetourName = nameof(LoadIndoorsDetour))]
+    private Hook<LoadIndoorsDelegate>? loadIndoorsHook;
+    
     // private delegate GraphicsConfig* CullConeDelegate(IntPtr a1);
     //
     // [Signature("48 89 5C 24 ?? 56 48 83 EC ?? 33 F6 48 8B D9 89 B1 ?? ?? ?? ?? 48 8B 05", DetourName = nameof(DetourCullCone))]
@@ -26,33 +32,77 @@ public unsafe class CullHook : IDisposable
     public CullHook()
     {
         Plugin.GameInteropProvider.InitializeFromAttributes(this);
-        cullHook?.Enable();
+        // cullHook?.Enable();
+        loadIndoorsHook?.Enable();
         // cullCone?.Enable();
+        
+        DisableCull();
 
-        Plugin.Log.Debug("Enabled hook!");
+        Plugin.Log.Verbose("Enabled CullHook!");
     }
 
-    public byte DetourCull(BgObject* a1, int a2, uint a3, float a4)
+    private static void DisableCull()
     {
-        //Plugin.Log.Debug($"{a1}, {a2}, {a3}, {a4}");
+        var man = HousingManager.Instance();
+        if (man == null || !man->IsInside()) return;
         
-        try
-        {
-            var man = HousingManager.Instance();
-            if (man != null && man->IsInside())
-            {
-                var ex = (BgObjectEx*)a1;
-                ex->Visibility = 0xFFFF;
-                return cullHook!.Original(a1, a2, a3, a4);
-            }
-        }
-        catch (Exception e)
-        {
-            Plugin.Log.Error($"{e}");
-        }
-        
-        return cullHook!.Original(a1, a2, a3, a4);
+        var config = (GraphicsConfigEx*)GraphicsConfig.Instance();
+        if (config == null) return;
+
+        config->IsInside = false; // just tell the game we're outside duh
+
+        Plugin.Framework.Run(RedrawObjects); // redraw objects in case they were already culled
     }
+    
+    private static void RedrawObjects()
+    {
+        var man = HousingManager.Instance();
+        if (man == null) return;
+
+        var furnitureMan = man->GetFurnitureManager();
+        
+        foreach (ref var ptr in furnitureMan->ObjectManager.ObjectArray.Objects)
+        {
+            var gameObject = ptr.Value;
+            if (gameObject == null) continue;
+            
+            gameObject->DisableDraw();
+            
+            Plugin.Log.Verbose($"Redrawing {gameObject->NameString}");
+        }
+    }
+
+    // public byte CullDetour(BgObject* a1, int a2, uint a3, float a4)
+    // {
+    //     //Plugin.Log.Debug($"{a1}, {a2}, {a3}, {a4}");
+    //     
+    //     try
+    //     {
+    //         var man = HousingManager.Instance();
+    //         if (man != null && (man->IsInside() || man->IsOutside()))
+    //         {
+    //             var ex = (BgObjectEx*)a1;
+    //             ex->Visibility = 0xFFFF;
+    //             return cullHook!.Original(a1, a2, a3, a4);
+    //         }
+    //     }
+    //     catch (Exception e)
+    //     {
+    //         Plugin.Log.Error($"{e}");
+    //     }
+    //     
+    //     return cullHook!.Original(a1, a2, a3, a4);
+    // }
+
+    public void LoadIndoorsDetour( IntPtr a1, int a2, CStringPointer a3, IntPtr a4, int a5, int a6, int a7, IntPtr a8, int a9)
+    {
+        loadIndoorsHook!.Original(a1, a2, a3, a4, a5, a6, a7, a8, a9);
+        
+        Plugin.Log.Debug($"{a3}");
+        
+        DisableCull();
+    }
+    
     //
     // public GraphicsConfig* DetourCullCone(IntPtr a1)
     // {
@@ -67,28 +117,13 @@ public unsafe class CullHook : IDisposable
     //     
     //     return cullCone!.Original(a1);
     // }
-
-    private void FixObjects()
-    {
-        var man = HousingManager.Instance();
-        if (man == null && !man->IsInside()) return;
-
-        var furnitureMan = man->GetFurnitureManager();
-        
-        foreach (ref var ptr in furnitureMan->ObjectManager.ObjectArray.Objects)
-        {
-            var gameObject = ptr.Value;
-            if (gameObject == null) continue;
-            
-            gameObject->DisableDraw();
-        }
-    }
     
     public void Dispose()
     {
-        cullHook?.Dispose();
-        // cullCone?.Dispose();
+        // cullHook?.Dispose();
+        loadIndoorsHook?.Dispose();
         
-        FixObjects();
+        Plugin.Framework.Run(RedrawObjects);
+        Plugin.Log.Verbose("CullHook disposed");
     }
 }
--- HousingCullFix/Plugin.cs
+++ HousingCullFix/Plugin.cs
@@ -6,25 +6,15 @@ namespace HousingCullFix;
 
 public sealed class Plugin : IDalamudPlugin
 {
-    [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
-    [PluginService] internal static ITextureProvider TextureProvider { get; private set; } = null!;
-    [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
-    [PluginService] internal static IClientState ClientState { get; private set; } = null!;
-    [PluginService] internal static IPlayerState PlayerState { get; private set; } = null!;
-    [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
     [PluginService] internal static IPluginLog Log { get; private set; } = null!;
     [PluginService] internal static IGameInteropProvider GameInteropProvider { get; private set; } = null!;
-    [PluginService] internal static IChatGui ChatGui { get; private set; } = null!;
+    [PluginService] internal static IFramework Framework { get; private set; } = null!;
+
+    private CullHook CullHook { get; init; } = new();
     
-    private CullHook CullHook { get; init; }
-
-    public Plugin()
-    {
-        CullHook = new CullHook();
-    }
-
     public void Dispose()
     {
         CullHook.Dispose();
+        Log.Verbose("Plugin disposed.");
     }
 }
