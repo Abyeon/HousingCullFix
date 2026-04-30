2148ef096dea77de36c1a2307c09d0c0a4743198
Abyeon <wpseals@gmail.com>
2026-04-30 13:55:44 -0400

    Re-enable culling for objects after dispose

--- HousingCullFix/CullHook.cs
+++ HousingCullFix/CullHook.cs
@@ -2,7 +2,10 @@
 using Dalamud.Hooking;
 using Dalamud.Utility.Signatures;
 using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.Game.Object;
+using FFXIVClientStructs.FFXIV.Client.Graphics.Render;
 using FFXIVClientStructs.FFXIV.Client.Graphics.Scene;
+using FFXIVClientStructs.FFXIV.Client.LayoutEngine.Layer;
 using HousingCullFix.Structs;
 
 namespace HousingCullFix;
@@ -14,11 +17,17 @@ public unsafe class CullHook : IDisposable
     
     [Signature("40 55 56 57 41 55 41 56 48 8D 6C 24 ?? 48 81 EC ?? ?? ?? ?? 0F 29 B4 24", DetourName = nameof(DetourCull))]
     private Hook<CullDelegate>? cullHook;
+
+    // private delegate GraphicsConfig* CullConeDelegate(IntPtr a1);
+    //
+    // [Signature("48 89 5C 24 ?? 56 48 83 EC ?? 33 F6 48 8B D9 89 B1 ?? ?? ?? ?? 48 8B 05", DetourName = nameof(DetourCullCone))]
+    // private Hook<CullConeDelegate>? cullCone;
     
     public CullHook()
     {
         Plugin.GameInteropProvider.InitializeFromAttributes(this);
         cullHook?.Enable();
+        // cullCone?.Enable();
 
         Plugin.Log.Debug("Enabled hook!");
     }
@@ -44,9 +53,42 @@ public unsafe class CullHook : IDisposable
         
         return cullHook!.Original(a1, a2, a3, a4);
     }
+    //
+    // public GraphicsConfig* DetourCullCone(IntPtr a1)
+    // {
+    //     try
+    //     {
+    //         Plugin.Log.Debug($"{a1:X}");
+    //     }
+    //     catch (Exception e)
+    //     {
+    //         Plugin.Log.Error($"{e}");
+    //     }
+    //     
+    //     return cullCone!.Original(a1);
+    // }
+
+    private void FixObjects()
+    {
+        var man = HousingManager.Instance();
+        if (man == null && !man->IsInside()) return;
+
+        var furnitureMan = man->GetFurnitureManager();
+        
+        foreach (ref var ptr in furnitureMan->ObjectManager.ObjectArray.Objects)
+        {
+            var gameObject = ptr.Value;
+            if (gameObject == null) continue;
+            
+            gameObject->DisableDraw();
+        }
+    }
     
     public void Dispose()
     {
         cullHook?.Dispose();
+        // cullCone?.Dispose();
+        
+        FixObjects();
     }
 }
